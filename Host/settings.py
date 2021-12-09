fcf627232b51431b9607478cf2ff441ce4c9e32e
Yash Lohakare <yashlohakare48@outlook.com>
2021-12-09 12:31:52 +0530

    minor error 0.1

--- Host/settings.py
+++ Host/settings.py
@@ -84,9 +84,7 @@ WSGI_APPLICATION = 'Host.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        dj_database_url.config(default=config('DATABASE_URL'))
-    }
+    'default': dj_database_url.config(default=config('DATABASE_URL'))
 }
 
 
